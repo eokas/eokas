95b6092fc5cc505a109a496e257636d5d22fe81d
Qilin <qilin.cheng@foxmail.com>
2025-04-09 00:28:03 +0800

    [Native] Add: new api for dialog and paste-board

--- modules/native/Windows/dialogs.cpp
+++ modules/native/Windows/dialogs.cpp
@@ -7,12 +7,15 @@
 #include <wrl.h>
 using namespace Microsoft::WRL;
 
-namespace eokas {
-    
-    static bool GetStringFromShellItem(String& strResult, const ComPtr<IShellItem>& pItem) {
-        if (pItem.Get() != nullptr) {
+namespace eokas
+{
+    static bool GetStringFromShellItem(String& strResult, const ComPtr<IShellItem>& pItem)
+    {
+        if (pItem.Get() != nullptr)
+        {
             PWSTR pszWStr = NULL;
-            if (SUCCEEDED(pItem->GetDisplayName(SIGDN_FILESYSPATH, &pszWStr))) {
+            if (SUCCEEDED(pItem->GetDisplayName(SIGDN_FILESYSPATH, &pszWStr)))
+            {
                 strResult = String::unicodeToUtf8(pszWStr, false);
                 CoTaskMemFree(pszWStr);
                 return true;
@@ -20,57 +23,65 @@ namespace eokas {
         }
         return false;
     }
-    
-    bool Dialogs::OpenFileDialog(String& selectedPath, const std::map<String, String>& filters) {
+
+    bool Dialogs::OpenFileDialog(String& selectedPath, const std::map<String, String>& filters)
+    {
         ComPtr<IFileDialog> pFileDialog = NULL;
-        if(FAILED(CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&pFileDialog)))) {
+        if(FAILED(CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&pFileDialog))))
+        {
             return false;
         }
-        
+
         DWORD dwOptions = 0;
         pFileDialog->GetOptions(&dwOptions);
         pFileDialog->SetOptions(dwOptions | FOS_FORCEFILESYSTEM | FOS_FILEMUSTEXIST | FOS_ALLOWMULTISELECT);
-        
+
         std::map<std::wstring, std::wstring> filtersW;
-        for(auto& entry : filters) {
+        for(auto& entry : filters)
+        {
             std::wstring name = String::utf8ToUnicode(entry.first.cstr(), false);
             std::wstring spec = String::utf8ToUnicode(entry.second.cstr(), false);
             filtersW.insert(std::make_pair(name, spec));
         }
-        
+
         std::vector<COMDLG_FILTERSPEC> filterTypes = {};
-        for(auto& entry : filtersW) {
+        for(auto& entry : filtersW)
+        {
             auto& filter = filterTypes.emplace_back();
             filter.pszName = entry.first.c_str();
             filter.pszSpec = entry.second.c_str();
         }
-        
+
         pFileDialog->SetFileTypes(filterTypes.size(), filterTypes.data());
         pFileDialog->SetFileTypeIndex(1);
-        
+
         HWND hwndOwner = nullptr;
         pFileDialog->Show(hwndOwner);
-        
+
         ComPtr<IShellItem> pItem = nullptr;
         pFileDialog->GetResult(&pItem);
-        
+
         return GetStringFromShellItem(selectedPath, pItem);
     }
-    
-    bool Dialogs::OpenFolderDialog(String& selectedPath, const String& defaultPath) {
+
+    bool Dialogs::OpenFolderDialog(String& selectedPath, const String& defaultPath)
+    {
         ComPtr<IFileOpenDialog> pFileDialog = nullptr;
-        if(FAILED(CoCreateInstance(CLSID_FileOpenDialog, nullptr, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&pFileDialog)))) {
+        if(FAILED(CoCreateInstance(CLSID_FileOpenDialog, nullptr, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&pFileDialog))))
+        {
             return false;
         }
-        
+
         DWORD dwOptions;
         pFileDialog->GetOptions(&dwOptions);
         pFileDialog->SetOptions(dwOptions | FOS_FORCEFILESYSTEM | FOS_PICKFOLDERS | FOS_PATHMUSTEXIST);
-        if(!defaultPath.isEmpty()) {
+        if(!defaultPath.isEmpty())
+        {
             std::wstring defaultPathW = String::utf8ToUnicode(defaultPath.cstr(), false);
             ComPtr<IShellItem> pDefaultFolderItem = nullptr;
             PCWSTR pszBaseLocation = defaultPathW.c_str();
-            if(SUCCEEDED(SHCreateItemFromParsingName(pszBaseLocation, nullptr, IID_PPV_ARGS(&pDefaultFolderItem)))) {
+            if(SUCCEEDED(SHCreateItemFromParsingName(pszBaseLocation, nullptr, IID_PPV_ARGS(&pDefaultFolderItem))))
+            {
                 pFileDialog->SetDefaultFolder(pDefaultFolderItem.Get());
             }
         }
--- modules/native/macOS/dialogs.mm
+++ modules/native/macOS/dialogs.mm
@@ -7,11 +7,51 @@
 
 namespace eokas
 {
-    bool Dialogs::OpenFileDialog(String& selectedPath, const std::map<String, String>& filters) {
+    void Dialogs::alert(const String& title, const String& message)
+    {
+        @autoreleasepool {
+            NSString* titleStr = [NSString stringWithUTF8String: title.cstr()];
+            NSString* messageStr = [NSString stringWithUTF8String: message.cstr()];
+
+            NSAlert *alert = [[NSAlert alloc] init];
+            [alert setMessageText: titleStr];
+            [alert setInformativeText: messageStr];
+            [alert addButtonWithTitle:@"Ok"];
+            [alert runModal];
+        }
+    }
+
+    int Dialogs::confirm(const String& title, const String& message)
+    {
+        @autoreleasepool {
+            NSString* titleStr = [NSString stringWithUTF8String: title.cstr()];
+            NSString* messageStr = [NSString stringWithUTF8String: message.cstr()];
+
+            NSAlert *alert = [[NSAlert alloc] init];
+            [alert setMessageText: titleStr];
+            [alert setInformativeText: messageStr];
+            [alert setAlertStyle:NSAlertStyleWarning];
+            [alert addButtonWithTitle:@"Yes"];
+            [alert addButtonWithTitle:@"No"];
+            [alert addButtonWithTitle:@"Cancel"];
+
+            NSInteger ret = [alert runModal];
+            switch (ret)
+            {
+                case NSAlertFirstButtonReturn: return 1;
+                case NSAlertSecondButtonReturn: return -1;
+                case NSAlertThirdButtonReturn: return 0;
+                default: return 0;
+            }
+        }
+    }
+
+    bool Dialogs::openFileDialog(String& selectedPath, const std::map<String, String>& filters)
+    {
         @autoreleasepool {
             NSOpenPanel *panel = [NSOpenPanel openPanel];
             panel.message = @"Open";
-            panel.prompt = @"OK";
+            panel.prompt = @"Ok";
             panel.canChooseFiles = YES;
             panel.allowsMultipleSelection = NO;
 
@@ -24,7 +64,7 @@ namespace eokas
                 [allowedTypes addObject:extension];
             }
             panel.allowedFileTypes = [allowedTypes copy];
-            
+
             if ([panel runModal] == NSModalResponseOK) {
                 NSURL *fileURL = [[panel URLs] objectAtIndex:0];
                 selectedPath = [[fileURL path] UTF8String];
@@ -33,12 +73,13 @@ namespace eokas
         }
         return false;
     }
-    
-    bool Dialogs::OpenFolderDialog(String& selectedPath, const String& defaultPath) {
+
+    bool Dialogs::openFolderDialog(String& selectedPath, const String& defaultPath)
+    {
         @autoreleasepool {
             NSOpenPanel *panel = [NSOpenPanel openPanel];
             panel.message = @"Open";
-            panel.prompt = @"OK";
+            panel.prompt = @"Ok";
             panel.canChooseFiles = NO;
             panel.canChooseDirectories = YES;
             panel.allowsMultipleSelection = NO;
