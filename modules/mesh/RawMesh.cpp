f001ce1d3a89433a611d65351aed3fb214a70161
qilincheng <>
2025-04-19 16:37:12 +0800

    [Mesh] Add: RawMesh basic functions

--- modules/mesh/RawMesh.cpp
+++ modules/mesh/RawMesh.cpp
@@ -2,5 +2,85 @@
 
 namespace eokas
 {
-
+    void RawMesh::Attribute::alloc(const CornerID& id)
+    {
+        const size_t corners = this->buffer.size() / this->stride;
+        while(id.value() >= corners)
+        {
+            for(u32_t i = 0; i < this->stride; i++)
+            {
+                this->buffer.emplace_back(0);
+            }
+        }
+    }
+    
+    void RawMesh::Attribute::get(const CornerID& id, Vector2& val)
+    {
+        u8_t* ptr = this->buffer.data() + id.value() * stride;
+        memcpy(&val, ptr, sizeof(val));
+    }
+    
+    void RawMesh::Attribute::get(const CornerID& id, Vector3& val)
+    {
+        u8_t* ptr = this->buffer.data() + id.value() * stride;
+        memcpy(&val, ptr, sizeof(val));
+    }
+    
+    void RawMesh::Attribute::get(const CornerID& id, Color& val)
+    {
+        u8_t* ptr = this->buffer.data() + id.value() * stride;
+        memcpy(&val, ptr, sizeof(val));
+    }
+    
+    void RawMesh::Attribute::set(const CornerID& id, const Vector2& val)
+    {
+        u8_t* ptr = this->buffer.data() + id.value() * stride;
+        memcpy(&ptr, &val, sizeof(val));
+    }
+    
+    void RawMesh::Attribute::set(const CornerID& id, const Vector3& val)
+    {
+        u8_t* ptr = this->buffer.data() + id.value() * stride;
+        memcpy(&ptr, &val, sizeof(val));
+    }
+    
+    void RawMesh::Attribute::set(const CornerID& id, const Color& val)
+    {
+        u8_t* ptr = this->buffer.data() + id.value() * stride;
+        memcpy(&ptr, &val, sizeof(val));
+    }
+    
+    VertexID RawMesh::addVertex(const Vertex& v)
+    {
+        this->vertices.push_back(v);
+        return this->vertices.size() - 1;
+    }
+    
+    CornerID RawMesh::addCorner(const VertexID& v)
+    {
+        Corner& corner = this->corners.emplace_back();
+        corner.vertexId = v;
+        
+        CornerID cornerId = this->corners.size() - 1;
+        
+        for(Attribute& attr : this->attributes)
+        {
+            attr.alloc(cornerId);
+        }
+        
+        return cornerId;
+    }
+    
+    AttributeID RawMesh::addAttribute(u32_t stride)
+    {
+        Attribute& attr = this->attributes.emplace_back();
+        attr.stride = stride;
+        return this->attributes.size() - 1;
+    }
+    
+    RawMesh::Attribute& RawMesh::getAttribute(const AttributeID& attributeId)
+    {
+        return this->attributes.at(attributeId.value());
+    }
+    
 }
--- modules/mesh/RawMesh.h
+++ modules/mesh/RawMesh.h
@@ -1,11 +1,71 @@
 #ifndef _EOKAS_MESH_RAWMESH_H_
 #define _EOKAS_MESH_RAWMESH_H_
 
-#include "base/main.h"
+#include "./header.h"
 
 namespace eokas
 {
+    class RawMesh
+    {
+    public:
+        struct Vertex
+        {
+            f32_t x;
+            f32_t y;
+            f32_t z;
+        };
+        
+        struct Corner
+        {
+            VertexID vertexId;
+        };
+        
+        struct Attribute
+        {
+            u32_t stride;
+            std::vector<u8_t> buffer;
+            
+            void alloc(const CornerID& id);
+            
+            void get(const CornerID& id, Vector2& val);
+            void get(const CornerID& id, Vector3& val);
+            void get(const CornerID& id, Color& val);
+            
+            void set(const CornerID& id, const Vector2& val);
+            void set(const CornerID& id, const Vector3& val);
+            void set(const CornerID& id, const Color& val);
+        };
+        
+        struct Triangle
+        {
+            CornerID c0;
+            CornerID c1;
+            CornerID c2;
+        };
+        
+        struct Polygon
+        {
+            std::vector<CornerID> corners;
+        };
+        
+        struct Section
+        {
+            std::vector<PolygonID> polygons;
+        };
+        
+        VertexID addVertex(const Vertex& v);
+        CornerID addCorner(const VertexID& v);
+        AttributeID addAttribute(u32_t stride);
+        Attribute& getAttribute(const AttributeID& attributeId);
 
+    private:
+        std::vector<Vertex> vertices;
+        std::vector<Corner> corners;
+        std::vector<Attribute> attributes;
+        std::vector<Triangle> triangles;
+        std::vector<Polygon> polygons;
+        std::vector<Section> sections;
+    };
 }
 
 #endif//_EOKAS_MESH_RAWMESH_H_
